9a97d6a1b7cc9a9cc89a09857cd9265b29668265
cedric <cedric-perrotin_student2021@wilder.school>
2021-03-31 19:02:17 +0200

    thanks.php

--- form.php
+++ form.php
@@ -1,63 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
+    foreach ($errors as $error) {
+        echo "<p class='error'>" . $error . "</p>" . PHP_EOL;
+    }
+    ?>
 
- echo '<form  action="thanks.php"  method="post">
-
-            <div>
-
-            <label  for="nom">Nom :</label>
-
-            <input  type="text"  id="nom"  name="user_name">
-
-            </div>
-            <div>
-
+        <form  action="thanks.php"  method="post">
+            <label  for="name">Nom :</label>
+            <input  type="text"  id="name"  name="name">
             <label for="prenom">Prénom :</label>
-
-            <input type="text" id="prenom"  name="user_firstname">
-
-            </div>
-
-            <div>
-
-            <label  for="courriel">Courriel :</label>
-
-            <input  type="email"  id="courriel"  name="user_email">
-
-            </div>
-
-            <div>
-
+            <input type="text" id="firstname"  name="firstname">
+            <label  for="mail">Email :</label>
+            <input  type="email"  id="mail"  name="mail">
             <label for="tel">Téléphone :</label>
-
-            <input type="tel" id="tel" name="user_tel">
-
-            </div>
-
-            <div>
-
+            <input type="tel" id="tel" name="tel">
             <select name="list" size="1">
             <option>info
             <option>demande
             <option>réclamation
             </select>
-
-            </div>
-
-            <div>
-
             <label  for="message">Message :</label>
-
-            <textarea  id="message"  name="user_message"></textarea>
-
-            </div>
-
-            <div  class="button">
-
+            <textarea  id="message"  name="message"></textarea>
             <button  type="submit">Envoyer votre message</button>
 
-            </div>
+        </form>
+    
+</body>
+</html>
 
-        </form>';
 
       
         
@@ -65,4 +43,3 @@
 
 
 
-?>
--- thanks.php
+++ thanks.php
@@ -1,27 +1,56 @@
 <?php
 
 
-$nom = $_POST["user_name"];
-$prenom = $_POST["user_firstname"];
-$email = $_POST["user_email"];
-$tel = $_POST["user_tel"];
+$nom = $_POST["name"];
+$prenom = $_POST["firstname"];
+$mail = $_POST["mail"];
+$tel = $_POST["tel"];
 $list = $_POST["list"];
-$message = $_POST["user_message"];
+$message = $_POST["message"];
+
+$errors = [];
+
+if (
+    filter_has_var(INPUT_POST, "name") &&
+    filter_has_var(INPUT_POST, "firstname") &&
+    filter_has_var(INPUT_POST, "mail") &&
+    filter_has_var(INPUT_POST, "tel") &&
+    filter_has_var(INPUT_POST, "list") &&
+    filter_has_var(INPUT_POST, "message")
+){ 
+    $nameSize = strlen($_POST["name"]);
+    if ($nameSize < 1){
+        $errors[] = "name too short";
+    }
+    if ($nameSize > 255){
+        $errors[] = "name too long";
+    }
+    $firstnameSize = strlen($_POST["firstname"]);
+    if ($firstnameSize < 1){
+        $errors[] = "firstname too short";
+    }
+    if ($firstnameSize > 100){
+        $errors[] = "firstname too long";
+    }
+    if (strlen($_POST["message"]) < 1){
+        $errors[] = "message too short";
+    }
+    if (empty($mail)){
+        $errors[] = "email not valided";
+    }
+    
+}
 
 
 
 include 'form.php';
 
 
-
-
 echo '<p>Merci '. $prenom . $nom . ' de nous avoir contacté à propos de ' . $list .
 
-' Un de nos conseiller vous contactera soit à l’adresse ' . $email . ' ou par téléphone au ' .$tel .' dans les plus brefs délais pour traiter votre demande : 
+' Un de nos conseiller vous contactera soit à l’adresse ' . $mail . ' ou par téléphone au ' .$tel .' dans les plus brefs délais pour traiter votre demande : 
 
 ' . $message . '</p>';
-
-
 
 
 
